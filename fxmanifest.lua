4b5bd1e099365bfc3b3dfd64010c1afe1847d840
Crew <97802269+CrewDevelopement@users.noreply.github.com>
2022-09-08 19:33:33 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Crew Developement'
+description 'Vape Shop Job made for Homeland RP'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+	'client/main.lua',
+	'client/menus.lua'
+}
+
+server_script 'server/*.lua'

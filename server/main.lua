0d132332f8a919bfd919d65d29c01f82eacaff8c
Crew <97802269+CrewDevelopement@users.noreply.github.com>
2022-09-08 19:31:37 -0400

    Create main.lua

---
+++ server/main.lua
@@ -0,0 +1,55 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+RegisterServerEvent("crew-vapejob:bill:player")
+AddEventHandler("crew-vapejob:bill:player", function(playerId, amount)
+        local biller = QBCore.Functions.GetPlayer(source)
+        local billed = QBCore.Functions.GetPlayer(tonumber(playerId))
+        local amount = tonumber(amount)
+        if biller.PlayerData.job.name == 'smoking' then
+            if billed ~= nil then
+                if biller.PlayerData.citizenid ~= billed.PlayerData.citizenid then
+                    if amount and amount > 0 then
+                       if Config.SQL == 'oxmysql' then
+                        exports.oxmysql:insert('INSERT INTO phone_invoices (citizenid, amount, society, sender) VALUES (:citizenid, :amount, :society, :sender)', {
+                            ['citizenid'] = billed.PlayerData.citizenid,
+                            ['amount'] = amount,
+                            ['society'] = biller.PlayerData.job.name,
+                            ['sender'] = biller.PlayerData.charinfo.firstname
+                        })
+                        else
+                          exports.ghmattimysql:execute('INSERT INTO phone_invoices (citizenid, amount, society, sender) VALUES (@citizenid, @amount, @society, @sender)', {
+                            ['@citizenid'] = billed.PlayerData.citizenid,
+                            ['@amount'] = amount,
+                            ['@society'] = biller.PlayerData.job.name,
+                            ['@sender'] = biller.PlayerData.charinfo.firstname
+                           })
+                       end
+                        TriggerClientEvent('qb-phone:RefreshPhone', billed.PlayerData.source)
+                        TriggerClientEvent('QBCore:Notify', source, 'Invoice Successfully Sent', 'success')
+                        TriggerClientEvent('QBCore:Notify', billed.PlayerData.source, 'New Invoice Received')
+                    else
+                        TriggerClientEvent('QBCore:Notify', source, 'Must Be A Valid Amount Above 0', 'error')
+                    end
+                else
+                    TriggerClientEvent('QBCore:Notify', source, 'You Cannot Bill Yourself', 'error')
+                end
+            else
+                TriggerClientEvent('QBCore:Notify', source, 'Player Not Online', 'error')
+            end
+        else
+            TriggerClientEvent('QBCore:Notify', source, 'No Access', 'error')
+        end
+end)
+
+QBCore.Functions.CreateCallback('crew-vapejob:server:get:ingredient', function(source, cb)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(source)
+    local common = Player.Functions.GetItemByName("commonjuice")
+    local bottle = Player.Functions.GetItemByName("juicebottle")
+    if common ~= nil and bottle ~= nil then
+        cb(true)
+    else
+        cb(false)
+    end
+end)
+

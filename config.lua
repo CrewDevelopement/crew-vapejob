05e632af85abc6d19100f4ff9f4c93658118859e
Crew <97802269+CrewDevelopement@users.noreply.github.com>
2022-09-08 19:32:49 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,27 @@
+Config = {}
+
+Config.SQL = 'oxmysql' --- oxmysql or ghmattisql
+
+
+Config.Items = {
+label = "Vape Shop Stock",
+    slots = 7,
+    items = {
+        [1] = {
+            name = "juicebottle",
+            price = 0,
+            amount = 5,
+            info = {},
+            type = "item",
+            slot = 1,
+        },
+        [2] = {
+            name = "commonjuice",
+            price = 0,
+            amount = 5,
+            info = {},
+            type = "item",
+            slot = 2,
+        },
+    }
+}
